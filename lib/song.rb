51d54df0cfd4b085cae0fbe623c27ee67f09396e
Simo Khouili <m.simokhouili@gmail.com>
2020-03-02 04:51:29 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -41,7 +41,7 @@ class Song
 
   def self.artist_count
       artist_count = {}
-    @@artists.each so |artist|
+    @@artists.each do |artist|
       if artist_count[artist]
         artist_count[artist] += 1
       else
