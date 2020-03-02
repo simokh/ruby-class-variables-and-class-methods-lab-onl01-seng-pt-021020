da757e06c0f13bc5098dc8e9f96f92af1c664c59
Simo Khouili <m.simokhouili@gmail.com>
2020-03-02 04:44:57 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -27,7 +27,7 @@ class Song
     @@genres.uniq
   end
 
-  def genre_count
+  def self.genre_count
     genre_count = {}
     @@genres.each do |genre|
       if genre_count[genre]
@@ -38,5 +38,8 @@ class Song
     end
     genre_count
   end
+
+
+
 
 end
