ec90aff3f01b4b10da022b20e97d8696ecfb5f25
Simo Khouili <m.simokhouili@gmail.com>
2020-03-02 03:39:03 +0000

    Automatically backed up by Learn

---
+++ lib/song.rb
@@ -0,0 +1,33 @@
+class Song
+
+  attr_accessor :name, :artist, :genre
+
+  @@genre = []
+  @@artists = []
+  @@count = 0
+  @@artist_count = {}
+  @@genre_count = {}
+
+  def initialize(name, artist, genre)
+    @name= song_name
+    
+
+  end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+end
