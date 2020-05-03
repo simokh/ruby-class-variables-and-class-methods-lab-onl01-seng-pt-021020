3ca7c629de7706fb1cd080ca24a2c34133b9e2b0
simokh <m.simokhouili@gmail.com>
2020-05-03 01:24:18 -0400

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -30,26 +30,26 @@ class Song
   end
 
   def self.genre_count
-    genre_count = {}
-    @@genres.each do |genre|
-      if genre_count[genre]
-         genre_count[genre] += 1
-      else
-        genre_count[genre] = 1
-      end
-    end
-    genre_count
+    genre_count = Hash.new(0)
+    @@genres.each {|genre| genre_count[genre] += 1}
+    # binding.pry
+    genre_count 
   end
+ 
 
   def self.artist_count
-      artist_count = {}
-    @@artists.each do |artist|
-      if artist_count[artist]
-        artist_count[artist] += 1
-      else
-        artist_count[artist] = 1
-      end
-    end
+    artist_count = Hash.new(0)
+    @@artists.each {|genre| artist_count[genre] += 1}
     artist_count
-  end
+  end 
 end
+
+
+  
+
+  
+
+
+
+
+
